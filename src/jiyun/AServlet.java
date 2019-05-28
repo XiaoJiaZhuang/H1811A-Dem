4e2529483e4a912d9f476eabd3b3380d1136ee3c
XiaoJiaZhuang <51111576+XiaoJiaZhuang@users.noreply.github.com>
2019-05-28 18:53:01 +0800

    123

--- src/jiyun/AServlet.java
+++ src/jiyun/AServlet.java
@@ -10,7 +10,7 @@ public class AServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
-
+        System.out.println("222");
     resp.getWriter().write("人退");
     }
 
